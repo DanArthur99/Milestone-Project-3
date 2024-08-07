a01a2da8fbeaa12d9884337865dd6bdba5f19ba6
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-08-07 22:56:14 +0100

    feat: sign up and login w/ password functionality added.

--- review_app/models.py
+++ review_app/models.py
@@ -3,16 +3,27 @@ from review_app import db, UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
+    password = db.Column(db.String, unique=False)
     username = db.Column(db.String(20), nullable=False, unique=True)
+    reviews = db.relationship("Review", backref="user", cascade="all, delete", lazy=True)
+    admin = db.Column(db.Boolean, default=False, nullable=False)
 
-class GearItem(db.Model):
+class Gear(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    brand_id = db.Column(db.Integer, db.ForeignKey("brand.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    gear = db.relationship("Gear", backref="category", cascade="all, delete", lazy=True)
 
 class Brand(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    brand_name = db.Column(db.String(25), unique=True, nullable=False)
+    gear = db.relationship("Gear", backref="brand", cascade="all, delete", lazy=True)
 
-class Reviews(db.Model):
+class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    gear_id = db.Column(db.Integer, db.ForeignKey("gear.id", ondelete="CASCADE"), nullable=False)
--- review_app/routes.py
+++ review_app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, flash
 from review_app import app, db, login_user, LoginManager, login_required, logout_user, current_user
-from review_app.models import User, GearItem, Category, Brand, Reviews
+from review_app.models import User, Gear, Category, Brand, Review
+import bcrypt
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -17,11 +18,16 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        entered_password = bytes(request.form.get("password"), "utf-8")
         user = User.query.filter_by(email=request.form.get("email")).first()
         if user:
-            login_user(user)
-            flash("Login successful!")
-            return redirect(url_for("dashboard"))
+            user_password = bytes(user.password, "utf-8")
+            if bcrypt.checkpw(entered_password, user_password):
+                login_user(user)
+                flash("Login successful!")
+                return redirect(url_for("dashboard"))
+            else:
+                flash("Password not recognised")
         else:
             flash("User not recognised")
     return render_template("login.html")
@@ -42,12 +48,23 @@ def dashboard():
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "POST":
-        user = User(
-            username=request.form.get("username"),
-            email=request.form.get("email_address")
-        )
-        db.session.add(user)
-        db.session.commit()
-        flash("Account created successfully")
-        return redirect(url_for("home"))
+        password = bytes(request.form.get("password"), "utf-8")
+        confirm_password = bytes(request.form.get("confirm_password"), "utf-8")
+        if password != confirm_password:
+            flash("Passwords don't match")
+        else:
+            hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt()) 
+            user = User(
+                username=request.form.get("username"),
+                email=request.form.get("email"),
+                password=hashed_pw.decode("utf-8")
+            )
+            db.session.add(user)
+            db.session.commit()
+            flash("Account created successfully")
+            return redirect(url_for("home"))
     return render_template("sign_up.html")
+
+@app.route("/add_review", methods=["GET", "POST"])
+def add_review():
+    return render_template("add_review.html")
