1842153cfc21c56e758ab97e0369baef45829ae3
Dan Arthur <148535574+DanArthur99@users.noreply.github.com>
2024-08-30 22:17:37 +0100

    fix: no value error fix

--- review_app/routes.py
+++ review_app/routes.py
@@ -479,6 +479,8 @@ def about_gear(id):
         gear_rating_total += float(review.review_rating)
     if len(reviews) > 0:
         gear_rating_mean = gear_rating_total / len(reviews)
+    else:
+        gear_rating_mean = None
     return render_template("about_gear.html", form=form, title=gear_item.name, gear=gear_item, reviews=reviews, score=gear_rating_mean)
 
 
